eca00287b5a3287c3a17d073983ea4f253308787
Alexander Guzyaev <al-programmist@yandex.ru>
2023-07-01 13:43:42 +0300

    Поправили валидацию bootstrap

--- js/main.js
+++ js/main.js
@@ -3,20 +3,14 @@
     'use strict'
 
     const forms = document.querySelectorAll('.needs-validation');
-    let setState = (form) => {
-        const inputs = form.querySelectorAll('.js-control-input');
-        console.log(inputs);
-    }
-
     Array.from(forms).forEach(form => {
-        setState(form);
         form.addEventListener('submit', event => {
             if (!form.checkValidity()) {
                 event.preventDefault()
                 event.stopPropagation()
             }
 
-            form.classList.add('was-validated')
+           form.classList.add('was-validated')
         }, false)
     })
 })()
