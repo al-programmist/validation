4c6349d7c8dfd3a0502f4ab9c104456509141fde
Alexander Guzyaev <al-programmist@yandex.ru>
2023-07-02 14:04:12 +0300

    native js - подготовили к валидации

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-// Валидация средствами Bootstrap
+
+/**
+ * Валидация средствами Bootstrap
+ */
 (() => {
     'use strict'
 
@@ -10,7 +13,65 @@
                 event.stopPropagation()
             }
 
-           form.classList.add('was-validated')
+            form.classList.add('was-validated')
         }, false)
     })
-})()
+})();
+
+
+const Validity = {
+    forms: [],
+    elements: [],
+    rules: {
+        email: {
+            pattern: /^(?!.{321})((?:\w+[.-])*\w{1,64})(@)(\w{1,255}(?:[.-]\w+)*\.\w{2,3})$/,
+            minlength: 5,
+            errorMessage: {
+                selector: '.invalid-tooltip',
+                pattern: 'Введите email в правильном формате. Это обязательное поле. Email не должен содержать спецсимвовов. Допустимые символы: -, ., _, @, а также строчные буквы и цифры.',
+                minlength: 'Поле не должно быть пустым',
+            }
+        },
+        name: {
+            pattern: /^[A-Za-zА-я\s\-]*$/,
+            minlength: 2,
+            errorMessage: {
+                pattern: 'Введите ваше имя в нужном формате. Оно должно начинаться с большой буквы и не содержать спецсимволов',
+                minlength: 'Имя не должно быть пустым. Это обязательное поле. Оно должно начинаться с большой буквы и не содержать спецсимволов'
+            }
+        }
+    },
+
+    selectors: {
+        validated: '.was-validated',
+        success: '.is-valid',
+        error: '.is-invalid',
+        message: '.invalid-tooltip'
+    },
+
+    setFormsValidation(selector) {
+        const forms = Array.from(document.querySelectorAll(selector));
+        if (forms.length) {
+            this.forms = forms;
+
+            forms.forEach((form, index) => {
+                let elements = Array.from(form.querySelectorAll('[required]'));
+                this.elements.push({
+                    index : elements
+                })
+            });
+        }
+    },
+
+    init(selector) {
+        if (selector && typeof (selector) === 'string') {
+            this.setFormsValidation(selector);
+            return;
+        }
+        console.log('no forms to validate!');
+    },
+};
+
+let start = Validity.init.bind(Validity);
+
+start('.js-form-native');
