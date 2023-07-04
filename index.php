94a05859c0e58f6f8cc06bfc1b42caa5afad4d7c
Alexander Guzyaev <al-programmist@yandex.ru>
2023-07-04 18:03:08 +0300

    Дописали скрипт на стороне сервера

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-   echo var_dump($_POST);
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $comment = $_POST['comment'];
+    $agree = $_POST['agree'];
+  
+    if (empty($username)) 
+        echo "Please enter name";
+    elseif(empty($email))
+        echo "Please enter email";
+      elseif(empty($agree))
+        echo "Please commit your data";    
+    else
+        echo "$username has already commited with conditions, has email $email and leaved comment: $comment";
+  }
 ?>
